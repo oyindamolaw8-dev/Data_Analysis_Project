58f7032a41722e1e7a6142bde0c98e51d9cf1149
Oyindamola Kenny <oyindamolaw8@gmail.com>
2025-11-27 11:29:17 +0100

    I added a new line of code

--- titans.work.py
+++ titans.work.py
@@ -8,3 +8,6 @@ df_for_cvs_original = pd.read_csv(r"c:\Users\User1\Downloads\student_performance
 df_cvs_copied = df_for_cvs_original.copy()
 
 print(df_cvs_copied)    # your total file
+
+print(df_cvs_copied.info()) # data type and memory (shows all information about your data
+# I added something
