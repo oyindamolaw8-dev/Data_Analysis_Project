fdabd07b3debdc6593c8e6e2ac715d0fcce2fe33
Oyindamola Kenny <oyindamolaw8@gmail.com>
2025-11-27 11:23:35 +0100

    My Analysis Journey

---
+++ titans.work.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+# Read from CVS & Excel Files
+df_for_cvs_original = pd.read_csv(r"c:\Users\User1\Downloads\student_performance_data.csv")
+
+
+#copied the orignal data into another variable
+df_cvs_copied = df_for_cvs_original.copy()
+
+print(df_cvs_copied)    # your total file
